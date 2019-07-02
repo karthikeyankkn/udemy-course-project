a5da35c9269388f6dfd28d9e4b5fdd28379cc38b
karthikeyankkn <connecttokkn@hotmail.com>
2019-07-02 16:01:39 +0530

    shopping list completed

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
-	recipes = [];
+	recipes: Recipe[] = [
+		new Recipe("test recipe 1", "test recipe description", "https://sweetcsdesigns.com/wp-content/uploads/2018/01/Best-Baked-Pork-Tenderloin-Picture.jpg"),
+		new Recipe("test recipe 2", "test recipe description", "https://cdn.pixabay.com/photo/2016/01/14/17/46/eat-1140371_960_720.jpg")
+	];
 	
+	
+
   constructor() { }
 
   ngOnInit() {
